d43560bc5544e17ea4a24fc50c16f3296d545c08
Mitch Ludkey <mludk92@hotmail.com>
2023-01-11 20:53:33 -0600

    ansered all the questions

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -14,6 +14,8 @@
 
 //CODE
 /*
+
+
 let number = 1;
 
 number++;
@@ -26,10 +28,10 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// we are declaring a varible called name and setting name to the string value of Dane 
+// we are then console logging a if else statment if name is equal to Mary and if it is true
+// if it is true then console.log Mary
+// else console how do you do? 
 
 //CODE
 /*
@@ -45,10 +47,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+//  sets a undefined varible called secret , then sets a var named code = 123
+// if code is = 123 meaning statment is true we then define var secret as string super 
+// and multiple int value of code by 2 code will now = 123 * 2 = 246
+// also if code is greater than 250 the  set var secret to string duper 
+// we then console log secret. 
 
 //CODE
 /*
@@ -70,11 +73,12 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
-
+//  define isStudent as boolean value of true , define age as int = 34 and zip as int = 55407
+// if two conditions are true those being isStuded = true and zip is > 80000 then we consoling youre a student on the west coast as string
+// else consolog if isstudent is false or age < 30 what are you hobbies 
+// else if isstudent is = true then log welcome to prime. we do not need to declare a and less then statment since > is handle above
+// if none of the conditions are true then log how about the weather 
+//we would for this for this statment log welcome to prime. 
 //CODE
 /*
 let isStudent = true;
@@ -130,13 +134,14 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
-let colorOne = 'red';
-let colorTwo = 'blue';
-let mix = true;
+let colorOne = 'blue'; // changed red to blue 
+let colorTwo = 'red'; // changed blue to red
+let mix = true; // this is correct 
 
 if (mix === true) {
   colorOne = 'purple';
-}
+  colorTwo = 'purple';
+} // added colorTwo var redefined for when mix is true 
 */
 
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
@@ -145,12 +150,12 @@ if (mix === true) {
 
 /*
 let temp = 40;
-const time = 4;
+let time = 4; // change from const to let -- i believe that a const is a never changing value 
 
-if (temp > 39 || time >= 4) {
+if (temp > 39 && time >= 4) {      
   console.log('throw away the food!');
 }
-*/
+*/ // changed || or to && and 
 
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
 // We check if age is greater than or equal to minAge 
@@ -158,12 +163,12 @@ if (temp > 39 || time >= 4) {
 
 /*
 let age = 21;
-const minAge = 21;
+let minAge = 21;  // const of minAge set to var let, 
 
-if(minAge <= age) {
+if(minAge >= age) {    // changed < operater to > 
   console.log('no entry');
 } else {
   console.log('enter');
 }
-*/
+*/  //the console.log if false might not need to be there based on the instructions but it also does not hurt to be there 
 
